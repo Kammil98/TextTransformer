b2b35958e8cf13c114ace7b21316ee7c01489ea7
Kammil98 <49918546+Kammil98@users.noreply.github.com>
2020-01-11 11:40:12 +0100

    Update TransformInterface.java
    Complete javadoc

--- src/main/java/pl/put/poznan/transformer/logic/TransformInterface.java
+++ src/main/java/pl/put/poznan/transformer/logic/TransformInterface.java
@@ -3,5 +3,8 @@ package pl.put.poznan.transformer.logic;
  * interface of Decorator
  * */
 public interface TransformInterface {
+	 /*
+	 * Function for text transformation
+	 * */
 	public String transform_text(String text);
 }
