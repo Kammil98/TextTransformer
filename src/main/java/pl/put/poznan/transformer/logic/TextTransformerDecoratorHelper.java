02b26f4aa53cf05436406be0f1137a2825e55cbf
Kammil98 <49918546+Kammil98@users.noreply.github.com>
2020-01-11 11:39:14 +0100

    Update TextTransformerDecoratorHelper.java
    Add javadoc

--- src/main/java/pl/put/poznan/transformer/logic/TextTransformerDecoratorHelper.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformerDecoratorHelper.java
@@ -1,7 +1,11 @@
 package pl.put.poznan.transformer.logic;
-
+/*
+* Class, which helps in creating Decorator
+*/
 public class TextTransformerDecoratorHelper implements TransformInterface {
-
+	/*
+	* function, which end decorator recursive calls of this function
+	*/
 	@Override
 	public String transform_text(String text) {
 		return text;
